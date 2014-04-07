bb98e5da31ff9730ce57a1a135a696a8ff615a8f
Dominic Cleal <dcleal@redhat.com>
2014-04-07 09:13:10 +0100

    Fix symlink existence test

--- setup_sources.sh
+++ setup_sources.sh
@@ -4,7 +4,7 @@ for spec in */*.spec; do
   d=$(dirname $spec)
   source0=$(spectool --list-files $spec | awk '{print $2}' | head -n1)
   sourcebase=$(basename "$source0")
-  [ -e $d/$sourcebase ] || continue
+  [ -h $d/$sourcebase ] || continue
   git annex whereis "$d/$sourcebase" | grep -q " web" && continue
   git annex addurl --file "$d/$sourcebase" "$source0"
 done
